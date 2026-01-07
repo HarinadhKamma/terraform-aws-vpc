92b692b2233abb8773b62fb620de173c70ae02b7
harinadh kamma <harinadkamma@gmail.com>
2026-01-07 14:16:02 +0530

    module

--- vpc-peering.tf
+++ vpc-peering.tf
@@ -22,7 +22,7 @@ resource "aws_route" "public_peering" {
 
 resource "aws_route" "default_peering" {
   count                     = var.vpc_peering_connection ? 1 : 0
-  route_table_id            = data.aws_vpc.default.main_route_table_id
+  route_table_id            = data.aws_route_table.main.id
   destination_cidr_block    = var.vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.main[count.index].id
 }
